ece4f7b17b8d753d62b582fd265e9fb20f3afd00
Najdanovszki <najdanovszki@gmail.com>
2020-01-23 16:36:48 +0100

    UI logs using API's db access

--- BlazorApp/BlazorAppUI/Startup.cs
+++ BlazorApp/BlazorAppUI/Startup.cs
@@ -31,11 +31,11 @@ namespace BlazorAppUI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<WebAPI.Data.ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<WebAPI.Data.ApplicationDbContext>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
