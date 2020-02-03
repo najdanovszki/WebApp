3ed542455bd88bc7c12b58df787b845657f885f6
Najdanovszki <najdanovszki@gmail.com>
2020-02-03 13:44:45 +0100

    Get user name

---
+++ BlazorApp/WebAPI/Controllers/UserAuthenticationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAuthenticationController : ControllerBase
+    {
+        // GET: api/UserAuthentication
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var userId = User.Identity.Name;
+            return new string[] { "value1", "value2", userId };
+        }
+
+        // GET: api/UserAuthentication/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/UserAuthentication
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/UserAuthentication/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
